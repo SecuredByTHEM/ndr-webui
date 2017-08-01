1d54148f838ab1bd90bc19aa6e0945c04d34807c
Michael Casadevall <michaelc@them.com>
2017-08-01 03:00:59 -0400

    Start wiring views together for various pages
    Signed-off-by: Michael Casadevall <michaelc@them.com>

--- ndr_webui/views.py
+++ ndr_webui/views.py
@@ -5,7 +5,23 @@ from ndr_webui import app
 @app.route('/index')
 
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
     return render_template('index.html',
                            title='Home',
                            user=user)
+
+# Site Overview
+@app.route('/site/<site_id>')
+def site_overview(site_id):
+    return render_template('index.html',
+                        title='Site Overview')
+
+# Site Syslog Info
+@app.route('/site/<site_id>/logs')
+def site_syslog(site_id):
+    return render_template('syslog.html',
+                        title='Site Logs')
+
+@app.route('/site/<site_id>/nmap')
+def site_nmap(site_id):
+    return render_template('nmap.html',
+                        title='Site NMAP Information')
