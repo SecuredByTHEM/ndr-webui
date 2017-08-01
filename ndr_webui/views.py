b4b4a36f5fc655fe8d385aaeebf8ca8d4f83491e
Michael Casadevall <michaelc@them.com>
2017-08-01 03:38:14 -0400

    Get the base framework in base and page navigation
    Signed-off-by: Michael Casadevall <michaelc@them.com>

--- ndr_webui/views.py
+++ ndr_webui/views.py
@@ -6,22 +6,27 @@ from ndr_webui import app
 
 def index():
     return render_template('index.html',
-                           title='Home',
-                           user=user)
+                           title='Home')
 
 # Site Overview
 @app.route('/site/<site_id>')
 def site_overview(site_id):
     return render_template('index.html',
-                        title='Site Overview')
+                        title='Site Overview',
+                        site_id=site_id,
+                        active_page='overview')
 
 # Site Syslog Info
 @app.route('/site/<site_id>/logs')
 def site_syslog(site_id):
     return render_template('syslog.html',
-                        title='Site Logs')
+                        title='Site Logs',
+                        site_id=site_id,
+                        active_page='logs')
 
 @app.route('/site/<site_id>/nmap')
 def site_nmap(site_id):
     return render_template('nmap.html',
-                        title='Site NMAP Information')
+                        title='Site NMAP Information',
+                        site_id=site_id,
+                        active_page='nmap')
